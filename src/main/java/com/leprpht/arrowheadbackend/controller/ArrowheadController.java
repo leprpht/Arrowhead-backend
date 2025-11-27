a4dae5d76c0e2b148ea6ce7e084e0168b621cae1
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-11-27 19:19:15 +0100

    Add prognosis endpoint

--- src/main/java/com/leprpht/arrowheadbackend/controller/ArrowheadController.java
+++ src/main/java/com/leprpht/arrowheadbackend/controller/ArrowheadController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -25,6 +28,23 @@ public class ArrowheadController {
             @RequestParam String from,
             @RequestParam String to) {
 
+        return fetchEnergyDataResponse(from, to);
+    }
+
+    @GetMapping("/prognosis")
+    public CompletableFuture<ResponseEntity<List<EnergyMixPeriod>>> getPrognosis() {
+        LocalDate today = LocalDate.now();
+
+        Instant from = today.atStartOfDay(ZoneOffset.UTC).toInstant();
+        Instant to = today.plusDays(2).atTime(23, 30).toInstant(ZoneOffset.UTC);
+
+        String fromStr = from.toString();
+        String toStr = to.toString();
+
+        return fetchEnergyDataResponse(fromStr, toStr);
+    }
+
+    private CompletableFuture<ResponseEntity<List<EnergyMixPeriod>>> fetchEnergyDataResponse(String from, String to) {
         return arrowheadService.fetchEnergyDataAsync(from, to)
                 .thenApply(ResponseEntity::ok)
                 .exceptionally(ex ->

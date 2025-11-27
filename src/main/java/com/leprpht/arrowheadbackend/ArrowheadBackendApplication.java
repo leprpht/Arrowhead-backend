cf958cea8987abe75a019fee3e0dda98fdd2f58c
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-11-27 12:13:05 +0100

    Initial commit

---
+++ src/main/java/com/leprpht/arrowheadbackend/ArrowheadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leprpht.arrowheadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrowheadBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArrowheadBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leprpht/arrowheadbackend/ArrowheadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leprpht.arrowheadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrowheadBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
